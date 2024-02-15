debb3c0510c3068e16c1e1c5af4b1b0d7d86a5be
Kevin Willot <66628344+kevinwillotnawezi@users.noreply.github.com>
2024-02-15 22:04:07 +0100

    Initial commit

---
+++ src/main/java/com/kwillot/usersmicroservices/UsersMicroservicesApplication.java
@@ -0,0 +1,13 @@
+package com.kwillot.usersmicroservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsersMicroservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsersMicroservicesApplication.class, args);
+	}
+
+}
